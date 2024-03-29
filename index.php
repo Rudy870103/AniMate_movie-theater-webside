636826282d0d462b95976538a03f682c2474a3fc
unknown <favoriteinfinite@gmail.com>
2024-03-29 23:44:27 +0800

    建立navbar

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>AniMate-Theater</title>
+</head>
+
+<body>
+    <div class="container-fluid" style="padding: 0;">
+
+        <aside>
+            <nav class="navbar navbar-expand-lg nav-bg p-3" >
+                <a class="navbar-brand" href="#" style="display:inline-block">
+                    <img src="./img/logo.png" style="width:70px">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav" >
+                    <ul class="navbar-nav" style="width:100%;padding-left: 70px;text-align:center">
+                        <li class="nav-item" style="width: 25%;">
+                            <a class="nav-link" href="#">首頁</a>
+                        </li>
+                        <li class="nav-item" style="width: 25%;">
+                            <a class="nav-link" href="#">時刻表</a>
+                        </li>
+                        <li class="nav-item" style="width: 25%;">
+                            <a class="nav-link" href="#">會員專區</a>
+                        </li>
+                        <li class="nav-item" style="width: 25%;">
+                            <a class="nav-link" href="#">活動公告</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </aside>
+
+
+
+        <main>
+
+        </main>
+
+
+
+
+        <footer></footer>
+    </div>
+</body>
+
+</html>
