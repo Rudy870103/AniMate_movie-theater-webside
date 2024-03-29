08a61683f8d22084d421cf6789147b76ee2a33ab
unknown <favoriteinfinite@gmail.com>
2024-03-30 00:33:39 +0800

    加入輪播圖

--- index.php
+++ index.php
@@ -14,14 +14,14 @@
     <div class="container-fluid" style="padding: 0;">
 
         <aside>
-            <nav class="navbar navbar-expand-lg nav-bg p-3" >
+            <nav class="navbar navbar-expand-lg nav-bg p-3">
                 <a class="navbar-brand" href="#" style="display:inline-block">
                     <img src="./img/logo.png" style="width:70px">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav" >
+                <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav" style="width:100%;padding-left: 70px;text-align:center">
                         <li class="nav-item" style="width: 25%;">
                             <a class="nav-link" href="#">首頁</a>
@@ -42,8 +42,45 @@
 
 
 
-        <main>
-
+        <main class="p-2">
+            <section class="left" style="height:200px;width:50%">
+                <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="./img/220px-Tenki_no_ko_Key_Visual.jpg" class="d-block" style="height: 60vh;margin:auto">
+                            <p class="p-3" style="color:white;text-align:center">
+                                電影名稱: <br>
+                                上映日期: <br>
+                                分級: <br>
+                            </p>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./img/171172784493906_P8841330.jpg" class="d-block" style="height: 60vh;margin:auto">
+                            <p class="p-3" style="color:white;text-align:center">
+                                電影名稱: <br>
+                                上映日期: <br>
+                                分級: <br>
+                            </p>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./img/poster-318x450.jpg" class="d-block" style="height: 60vh;margin:auto">
+                            <p class="p-3" style="color:white;text-align:center">
+                                電影名稱: <br>
+                                上映日期: <br>
+                                分級: <br>
+                            </p>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </section>
         </main>
 
 
