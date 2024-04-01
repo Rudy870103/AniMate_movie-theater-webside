f58ccacd2d010202ac4e0c8e1193c69cb5d3cc00
unknown <favoriteinfinite@gmail.com>
2024-04-01 17:02:59 +0800

    重新建立menu的頁面導向，修改rwd的顏色

---
+++ front/main.php
@@ -0,0 +1,38 @@
+<section class="left" style="height:200px;width:50%">
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./img/220px-Tenki_no_ko_Key_Visual.jpg" class="d-block" style="height: 60vh;margin:auto">
+                <p class="p-3" style="color:white;text-align:center">
+                    電影名稱: <br>
+                    上映日期: <br>
+                    分級: <br>
+                </p>
+            </div>
+            <div class="carousel-item">
+                <img src="./img/171172784493906_P8841330.jpg" class="d-block" style="height: 60vh;margin:auto">
+                <p class="p-3" style="color:white;text-align:center">
+                    電影名稱: <br>
+                    上映日期: <br>
+                    分級: <br>
+                </p>
+            </div>
+            <div class="carousel-item">
+                <img src="./img/poster-318x450.jpg" class="d-block" style="height: 60vh;margin:auto">
+                <p class="p-3" style="color:white;text-align:center">
+                    電影名稱: <br>
+                    上映日期: <br>
+                    分級: <br>
+                </p>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -12,28 +12,28 @@
 
 <body>
     <div class="container-fluid" style="padding: 0;">
-
+        <!-- navbar -->
         <aside>
-            <nav class="navbar navbar-expand-lg nav-bg p-3">
-                <a class="navbar-brand" href="#" style="display:inline-block">
+            <nav class="navbar navbar-expand-lg nav-bg p-3 navbar-dark">
+                <a class="navbar-brand" href="index.php" style="display:inline-block">
                     <img src="./img/logo.png" style="width:70px">
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button class="navbar-toggler" data-bs-theme="dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"  style="border-color:white"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav" style="width:100%;padding-left: 70px;text-align:center">
                         <li class="nav-item" style="width: 25%;">
-                            <a class="nav-link" href="#">首頁</a>
+                            <a class="nav-link" href="index.php">首頁</a>
                         </li>
                         <li class="nav-item" style="width: 25%;">
-                            <a class="nav-link" href="#">時刻表</a>
+                            <a class="nav-link" href="?do=movie_list">時刻表</a>
                         </li>
                         <li class="nav-item" style="width: 25%;">
-                            <a class="nav-link" href="#">會員專區</a>
+                            <a class="nav-link" href="?do=member">會員專區</a>
                         </li>
                         <li class="nav-item" style="width: 25%;">
-                            <a class="nav-link" href="#">活動公告</a>
+                            <a class="nav-link" href="?do=news">活動公告</a>
                         </li>
                     </ul>
                 </div>
@@ -41,46 +41,20 @@
         </aside>
 
 
-
+        <!-- 網頁內容 -->
         <main class="p-2">
-            <section class="left" style="height:200px;width:50%">
-                <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="./img/220px-Tenki_no_ko_Key_Visual.jpg" class="d-block" style="height: 60vh;margin:auto">
-                            <p class="p-3" style="color:white;text-align:center">
-                                電影名稱: <br>
-                                上映日期: <br>
-                                分級: <br>
-                            </p>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="./img/171172784493906_P8841330.jpg" class="d-block" style="height: 60vh;margin:auto">
-                            <p class="p-3" style="color:white;text-align:center">
-                                電影名稱: <br>
-                                上映日期: <br>
-                                分級: <br>
-                            </p>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="./img/poster-318x450.jpg" class="d-block" style="height: 60vh;margin:auto">
-                            <p class="p-3" style="color:white;text-align:center">
-                                電影名稱: <br>
-                                上映日期: <br>
-                                分級: <br>
-                            </p>
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </section>
+            <?php
+            $do=$_GET['do']??'main';
+            $file="./front/{$do}.php";
+
+            if(file_exists($file)){
+                include $file;
+            }else{
+                include "./front/main.php";
+            }
+            ?>
+
+
         </main>
 
 
