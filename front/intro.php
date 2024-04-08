52d413d1e574223455ee20e2eb39e22abdd908df
unknown <favoriteinfinite@gmail.com>
2024-04-08 14:21:38 +0800

    建立電影簡介頁面

--- front/intro.php
+++ front/intro.php
@@ -1,6 +1,36 @@
 <?php
-$movie=$Movie->find($_GET['id']);
+$movie = $Movie->find($_GET['id']);
 ?>
-<div>
-    <?=$movie['trailer'];?>
+<div class="container">
+    <div class="row">
+        <div class="col-6 mx-auto text-center">
+            <?= $movie['trailer']; ?>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-6 mx-auto text-center">
+            <img src="./img/<?= $movie['poster']; ?>" width="50%">
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-6 mx-auto">
+            <div class="text-center" style="font-size: 20px;font-weight:bold">
+                <?= $movie['name']; ?>
+            </div>
+            <div style="line-height: 30px;">
+                上映時間 : <?=$movie['ondate'];?><br>
+                類型 : <?=$movie['kind'];?><br>
+                片長 : <?=$movie['time'];?>分鐘<br>
+                分級 : <img src="./img/level<?=$movie['level'];?>.png" width="50px"><br>
+                導演 : <?=$movie['director'];?><br>
+                演員 : <?=$movie['actor'];?><br>
+                發行商 : <?=$movie['company'];?><br>
+                <hr>
+                <?=$movie['intro'];?>
+            </div>
+            <div class="text-center mt-3">
+                <button class="login-btn" onclick="location.href='?do=order&id=<?=$movie['id'];?>'">前往購票</button>
+            </div>
+        </div>
+    </div>
 </div>
