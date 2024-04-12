3c97953ebb0fe8edf388f8ddad1f55860411fce1
unknown <favoriteinfinite@gmail.com>
2024-04-12 22:50:56 +0800

    完成電影情報詳細內容

---
+++ front/news-content.php
@@ -0,0 +1,20 @@
+<?php
+$news = $News->find($_GET['id']);
+?>
+<div class="container">
+    <div class="row mt-3">
+        <div class="col-6 mx-auto text-center">
+            <img src="./img/<?= $news['img']; ?>" width="100%">
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-6 mx-auto">
+            <div style="font-size: 20px;font-weight:bold;line-height:150%">
+                <?= $news['title']; ?><br><br>
+            </div>
+            <div style="line-height: 30px;text-wrap:wrap;width:100%">
+                <?=nl2br($news['text']);?>
+            </div>
+        </div>
+    </div>
+</div>
